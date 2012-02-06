0ed6afeeb79d3a492d4a5cfce4b5e91e781a1a3a
Karthik Ram <karthik.ram@gmail.com>
2012-02-06 13:23:03 -0800

    added skeleton for first two functions

---
+++ ropensci_auth.r
@@ -0,0 +1,5 @@
+ropensci_auth(package=NULL)
+{
+# Handler for auth routines
+
+}
---
+++ ropensci_key.r
@@ -0,0 +1,5 @@
+ropensci_key<-function(package_name)
+{
+	#Check for key availablility in Options().
+	# handle error gracefully and return further options
+}
