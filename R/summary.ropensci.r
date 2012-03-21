eafe56c69baacdf3f95240a7b11f734c1d0badc0
Karthik Ram <karthik.ram@gmail.com>
2012-03-21 14:34:21 -0700

    added notes to summary method.

--- R/summary.ropensci.r
+++ R/summary.ropensci.r
@@ -5,4 +5,8 @@
 #' @S3method summary ropensci
 summary.ropensci <- function(dset, verbose = FALSE) {
     # function yet to be coded
-} 
+    # List the call (search terms)
+    # Then the data source
+    # Then a summary of records retrieved.
+    # If data source == Mendeley, do x.
+}
