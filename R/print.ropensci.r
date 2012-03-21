bbf1b4c2dcac982fdc1ebe97d7077239b22ac00d
Karthik Ram <karthik.ram@gmail.com>
2012-03-21 14:34:01 -0700

    Minor edit: formatting.

--- R/print.ropensci.r
+++ R/print.ropensci.r
@@ -3,11 +3,11 @@
 #' @rdname ropensci
 #' @S3method print ropensci
 print.ropensci <- function(ro_object, verbose = FALSE) {
-    ro_object$call <- ifelse(is.null(ro_object$call), "undefined call", 
+    ro_object$call <- ifelse(is.null(ro_object$call), "undefined call",
         ro_object$call)
-    ro_object$source <- ifelse(is.null(ro_object$source), "undefined call", 
+    ro_object$source <- ifelse(is.null(ro_object$source), "undefined call",
         ro_object$source)
-    cat("Results from ", ro_object$call, "to", ro_object$source, 
+    cat("Results from ", ro_object$call, "to", ro_object$source,
         "\n")
     if (is.null(ro_object$format)) {
         ro_object$format <- "Undefined"
@@ -15,10 +15,10 @@ print.ropensci <- function(ro_object, verbose = FALSE) {
     cat("Format:", ro_object$format, "\n")
     if (!empty(ro_object$data)) {
         head(data)
-        if (dim(data)[1] > 10) 
-            cat("Printed first 10 rows of", dim(data)[1], "\n")
+        if (dim(data)[1] > 10)
+            cat("Printed first 10 rows of ", dim(data)[1], "\n")
     }
     if (empty(ro_object$data)) {
         cat("No data in object", "\n")
     }
-} 
+}
